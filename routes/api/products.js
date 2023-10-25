e6995dac0f1b1de4d3d24add1dcbe0c85fe6e2f7
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-25 23:26:53 +0900

    Feat: multer를 이용해 이미지 post 구현

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const products = require('../../models/products');
 
-// 모든 상품의 정보를 얻는다.
+try {
+  if (!fs.existsSync('uploads')) {
+    // 폴더가 없으면 생성
+    fs.mkdirSync('uploads');
+  }
+} catch (error) {
+  console.error('Failed to create directory.');
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 router.get('/', (req, res) => {
   const allProduct = products.getProducts();
   res.send(allProduct);
 });
 
-// 새로운 게시물 등록
-router.post('/post', (req, res) => {
-  const { userId, productName, img, categories, count, price, discount, delivery, exchange, description, tags, size } =
-    req.body;
+router.post('/post', upload.single('photo'), (req, res) => {
+  const { userId, productName, categories, count, price, discount, delivery, exchange, description, tags, size } =
+    req.body.data;
+
+  console.log('here', req.body.data);
+
+  const img = req.file.filename;
   const productId = products.createProductId();
 
   products.createProduct({
