229a820c4097f095bd7d1c088d28c638a899436e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-20 04:30:22 +0900

    Comment: 불필요한 주석 제거

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-// const users = require('../../models/users');
 const users = require('../../models/controllers/userController');
 const products = require('../../models/controllers/productController');
 
