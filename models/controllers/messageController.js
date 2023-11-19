ce3f2b8828c54f01df75fa52b9c35b824f654ded
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-19 23:34:21 +0900

    Feat: UserId로 메세지 리스트 찾는 함수 구현

--- models/controllers/messageController.js
+++ models/controllers/messageController.js
@@ -2,8 +2,18 @@ let messages = require('../mock_data/messages');
 
 const getMessages = () => messages;
 
+const findMessageByUserId = userId =>
+  messages
+    .filter(message => message.buyerId === userId || message.sellerId === userId)
+    .map(message => ({
+      messageId: message.messageId,
+      partner: [message.buyerId, message.sellerId].find(id => id !== userId),
+      latestMessage: message.messages[message.messages.length - 1],
+    }))
+    .sort((a, b) => b.latestMessage.timestamp - a.latestMessage.timestamp);
+
 // messageId 해당 메세지를 찾는 함수.
-const findMessageById = messageId => messages.filter(message => message.messageId === +messageId);
+const findMessageByMessageId = messageId => messages.filter(message => message.messageId === +messageId);
 
 const updateMessage = (messageId, newMessage) => {
   messages = messages.map(message =>
@@ -15,6 +25,7 @@ const updateMessage = (messageId, newMessage) => {
 
 module.exports = {
   getMessages,
-  findMessageById,
+  findMessageByUserId,
+  findMessageByMessageId,
   updateMessage,
 };
