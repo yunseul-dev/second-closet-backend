eaed9e65fffe7f425380dee61cada84930425343
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-20 13:07:10 +0900

    Feat: messages api 추가

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const { PORT } = process.env;
 const auth = require('./routes/api/auth');
 const users = require('./routes/api/users');
 const products = require('./routes/api/products');
+const messages = require('./routes/api/messages');
 
 const corsOptions = {
   origin: process.env.CORS_CLIENT_URL,
@@ -24,5 +25,6 @@ app.use(cookieParser());
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 app.use('/api/products', products);
+app.use('/api/messages', messages);
 
 app.listen(PORT);
